5f84f42f4d08d091a72f6f8938b438f232d4dd73
mofas <fedt1234@hotmail.com>
2013-09-13 11:10:14 +0800

    add resizeable and icon

--- app/scripts/directives/ui.js
+++ app/scripts/directives/ui.js
@@ -1,5 +1,34 @@
 'use strict';
 
+
+var throttle = function(func, wait, options) {
+	var context, args, result;
+	var timeout = null;
+	var previous = 0;
+	options || (options = {});
+	var later = function() {
+		previous = options.leading === false ? 0 : new Date;
+		timeout = null;
+		result = func.apply(context, args);
+	};
+	return function() {
+		var now = new Date;
+		if (!previous && options.leading === false) previous = now;
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		if (remaining <= 0) {
+			clearTimeout(timeout);
+			timeout = null;
+			previous = now;
+			result = func.apply(context, args);
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+};
+
 var ui = angular.module('ui' , []);
 
 
@@ -13,47 +42,97 @@ ui.directive('draggable', ['$document' , function($document) {
 				cursor: 'pointer',
 				position: 'relative',
 			});
+			var mousemove = function(event) {
+				y = event.screenY - startY;
+				x = event.screenX - startX;				
+				element.css({
+					left : x+'px',
+					top : y+'px'
+				});
+			}
+
+			var mouseup = function(){
+				$document.unbind('mousemove', throttleMousemove);
+				$document.unbind('mouseup', mouseup);
+			}
+
+			var throttleMousemove = throttle(mousemove , 16);
+
 			element.on('mousedown', function(event) {				
 			    // Prevent default dragging of selected content
 			    event.preventDefault();
 			    startX = event.screenX - x;
 			    startY = event.screenY - y;
-			    $document.on('mousemove', mousemove);
+			    $document.on('mousemove', throttleMousemove);
 			    $document.on('mouseup', mouseup);
 			});
 
-			function mousemove(event) {
-				y = event.screenY - startY;
-				x = event.screenX - startX;				
-				element.css({
-                    left : x+'px',
-                    top : y+'px'
-                });
-			}
-
-			function mouseup() {
-				$document.unbind('mousemove', mousemove);
-				$document.unbind('mouseup', mouseup);
-			}
+			
 		}
 	}
 }]);
 
-ui.directive('resizeable', [function() {
+ui.directive('resizeable', [ '$document' , function($document) {
 
 	return {
 		// TODO        
 		priority : 2,
-        link : function(scope, element, attrs) {            
-            
-        },        
-        template:
-            '<div class="resizeable">' +
-                '<div class="top-border">' +
-            '</div>',
-        replace: true,
-        transclude: true
-    };
+		link : function(scope, element, attrs) {        
+			var startX = 0,
+				startY = 0,
+				x = 0,
+				y = 0,
+				rotateDegrees = 0,
+				oriW = 0,
+				oriH = 0;
+
+
+			var mousemove = function(event) {
+				
+				y = event.screenY - startY;
+				x = event.screenX - startX;
+				var stretchW = oriW+x;
+				var stretchH = oriH+y;
+
+				//min limit				
+				if(stretchW < 50){
+					stretchW = 50;
+				}
+				if(stretchH < 50){
+					stretchH = 50;
+				}
+				element.css({
+					'width': stretchW + "px",
+					'height': stretchH + "px"
+				});
+			}
+
+			var mouseup = function() {
+				$document.unbind('mousemove', mousemove);
+				$document.unbind('mouseup', mouseup);
+			}
+
+			angular.element(element[0].children[0]).on('mousedown', function(event) {			    
+				event.preventDefault();
+				event.stopPropagation();
+				oriW = element[0].clientWidth;
+				oriH = element[0].clientHeight;
+				startX = event.screenX;
+				startY = event.screenY;
+				$document.on('mousemove', mousemove);
+				$document.on('mouseup', mouseup);
+				rotateDegrees = element[0].getAttribute("rotation") || 0;
+			});
+
+		},        
+		template:
+		'<span class="resizeable">' + 
+			'<span class="btn btn-default resizeBtn" data-icon="&#xe000;"></span>' + 
+			'<span class="resizeableWrap" ng-transclude></span>' +
+		'</span>', 
+		replace: true,
+		transclude: true
+	};
 
 }]);
 
@@ -62,23 +141,15 @@ ui.directive('rotatable', [ '$document' , function($document) {
 
 	return {		
 		priority : 4,
-        link : function(scope, element, attrs) {            
-        	var startX = 0, startY = 0, x = 0, y = 0 , rotateDegrees = 0;        	        	
-    		angular.element(element[0].children[0].children[0]).on('mousedown', function(event) {			    
-			    event.preventDefault();
-			    event.stopPropagation();
-			    startX = event.screenX;
-			    startY = event.screenY;
-			    $document.on('mousemove', mousemove);
-			    $document.on('mouseup', mouseup);
-			});
+		link : function(scope, element, attrs) {            
+			var startX = 0, startY = 0, x = 0, y = 0 , rotateDegrees = 0;
 
-			function mousemove(event) {
+			var mousemove = function (event) {
 				y = event.screenY - startY;
 				x = event.screenX - startX;
-			    startX = event.screenX;
-			    startY = event.screenY;
-				var absPath = Math.sqrt(x*x + y*y);								
+				startX = event.screenX;
+				startY = event.screenY;
+				var rotateDelta = Math.floor((Math.sqrt(x*x + y*y)*3)/5);
 				if(rotateDegrees > 360){
 					rotateDegrees-= 360;
 				}
@@ -86,60 +157,76 @@ ui.directive('rotatable', [ '$document' , function($document) {
 					rotateDegrees+= 360;
 				}
 
-				if(rotateDegrees < 45 || rotateDegrees >= 315){
+				if(rotateDegrees < 45 || rotateDegrees >= 315){					
 					if(x+y > 0){
-						rotateDegrees+= absPath;
-					}					
+						rotateDegrees-= rotateDelta;
+					}
 					else{
-						rotateDegrees-= absPath;
+						rotateDegrees+= rotateDelta;
 					}
 				}				
 				else if(rotateDegrees >= 45 && rotateDegrees < 135){
 					if(x-y > 0){
-						rotateDegrees-= absPath;
+						rotateDegrees+= rotateDelta;
 					}
 					else{
-						rotateDegrees+= absPath;
+						rotateDegrees-= rotateDelta;
 					}
 				}
 				else if(rotateDegrees >= 135 && rotateDegrees < 225){
 					if(x+y > 0){
-						rotateDegrees-= absPath;
-					}
+						rotateDegrees+= rotateDelta;
+					}					
 					else{
-						rotateDegrees+= absPath;
+						rotateDegrees-= rotateDelta;
 					}
 				}
 				else if(rotateDegrees >= 225 || rotateDegrees < 315){
 					if(x-y > 0){
-						rotateDegrees+= absPath;
+						rotateDegrees-= rotateDelta;
 					}
 					else{
-						rotateDegrees-= absPath;
+						rotateDegrees+= rotateDelta;
 					}
 				}
 
 				element.css({
-                    '-moz-transform': 'rotate(' + rotateDegrees + 'deg)',
-                    '-webkit-transform': 'rotate(' + rotateDegrees + 'deg)',
-                    '-o-transform': 'rotate(' + rotateDegrees + 'deg)',
-                    '-ms-transform': 'rotate(' + rotateDegrees + 'deg)'
-                });
+					'-moz-transform': 'rotate(' + rotateDegrees + 'deg)',
+					'-webkit-transform': 'rotate(' + rotateDegrees + 'deg)',
+					'-o-transform': 'rotate(' + rotateDegrees + 'deg)',
+					'-ms-transform': 'rotate(' + rotateDegrees + 'deg)'
+				});
+
+				element[0].setAttribute("rotation", rotateDegrees);
 
 			}
 
-			function mouseup() {
-				$document.unbind('mousemove', mousemove);
+			var mouseup = function() {
+				$document.unbind('mousemove', throttleMousemove);
 				$document.unbind('mouseup', mouseup);
 			}
-        },        
-        template:
-            '<span class="rotatable">' + 
-            	'<span class="btn btn-default roteateBtn">@</span>' + 
-            	'<span class="rotatableWrap" ng-transclude></span>' +
-            '</span>',        
-        transclude: true
-    };    
+
+			var throttleMousemove = throttle(mousemove , 16);
+
+
+			angular.element(element[0].children[0].children[0]).on('mousedown', function(event) {			    
+				event.preventDefault();
+				event.stopPropagation();
+				startX = event.screenX;
+				startY = event.screenY;
+				$document.on('mousemove', throttleMousemove);
+				$document.on('mouseup', mouseup);
+			});
+			
+
+		},        
+		template:
+		'<span class="rotatable">' + 
+		'<span class="btn btn-default roteateBtn" data-icon="&#xe003;"></span>' + 
+		'<span class="rotatableWrap" ng-transclude></span>' +
+		'</span>',        
+		transclude: true
+	};    
 
 }]);
 
@@ -149,22 +236,22 @@ ui.directive('deleteable', [function() {
 	return {                
 		priority : 2,
 		scope : {},
-        link : function(scope, element, attrs) {                    	
-          	scope.del = function(e){
-          		element.addClass("del");
-          		var $element = element[0];
-          		setTimeout(function(){
-          			$element.parentNode.removeChild($element);
-          		} , 300);
-          	}  
-        },
-        template:
-            '<span class="deleteable">' + 
-            	'<span class="btn btn-default delBtn" ng-click="del();">&times</span>' + 
-            	'<span ng-transclude></span>' +
-            '</span>',            
-        transclude: true
-    };    
+		link : function(scope, element, attrs) {                    	
+			scope.del = function(e){
+				element.addClass("del");
+				var $element = element[0];
+				setTimeout(function(){
+					$element.parentNode.removeChild($element);
+				} , 300);
+			}  
+		},
+		template:
+		'<span class="deleteable">' + 
+		'<span class="btn btn-default delBtn" ng-click="del();" data-icon="&#xe002;"></span>' + 
+		'<span ng-transclude></span>' +
+		'</span>',            
+		transclude: true
+	};    
 
 }]);
 
@@ -173,24 +260,24 @@ ui.directive('selectable', [ '$document', function($document) {
 
 	return {                
 		priority : 1,
-        controller : 'selectableCtrl',
-        link : function(scope , element , attrs){
-        	element.addClass("selectable");
-        },
-        template: '<span class="inner" ng-transclude></span>',            
-        transclude: true
-    };
+		controller : 'selectableCtrl',
+		link : function(scope , element , attrs){
+			element.addClass("selectable");
+		},
+		template: '<span class="inner" ng-transclude></span>',            
+		transclude: true
+	};
 }]);
 
 ui.controller('selectableCtrl', ['$scope', '$element', 
-    function ($scope, $element) {    	      
-    	$scope.perviousSelected = null;    	    	
-    	$element.bind("click" , function(event){    		
-    		if($scope.perviousSelected != null){
-    			$scope.perviousSelected.removeClass("select");
-    		}
-    		$element.addClass("select");
-    		$scope.perviousSelected = $element;
-    	});
-    }
-]);
+	function ($scope, $element) {    	      
+		$scope.perviousSelected = null;    	    	
+		$element.bind("click" , function(event){    		
+			if($scope.perviousSelected != null){
+				$scope.perviousSelected.removeClass("select");
+			}
+			$element.addClass("select");
+			$scope.perviousSelected = $element;
+		});
+	}
+	]);
