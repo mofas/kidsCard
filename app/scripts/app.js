3abc3aadcf327016764fec2fb022247d804d6794
mofas <fedt1234@hotmail.com>
2013-10-29 10:46:15 +0800

    fix ie 10 overlay bug

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,8 @@ module = angular.module('kidsCardApp', [
   'kidsCardApp.controllers'
 ]);
 
+
+
 module.config([ '$routeProvider' , function ($routeProvider) {
   $routeProvider
     .when('/', {
@@ -73,4 +75,22 @@ module.config([ '$routeProvider' , function ($routeProvider) {
     .otherwise({
       redirectTo: '/'
     });
+}]);
+
+
+//check login status
+module.run(['$rootScope', '$location', function($rootScope, $location) {  
+  $rootScope.$on( "$locationChangeStart", function(event, next, current) {        
+    var route = next.substring(next.indexOf("#")+2 || 0 , next.length);
+    // replace checkLogin with your check function
+    var checkLogin = false;
+    if ( !checkLogin ) {
+      // no logged user, check route
+      // if ( route == "collectTask" || route == "cardFactory" || route == "giftBox") {
+      //   //redirect & do something        
+      //   alert("這個需要登入唷");
+      //   event.preventDefault();        
+      // }
+    }         
+  });
 }]);
